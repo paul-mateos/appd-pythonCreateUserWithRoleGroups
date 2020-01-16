13a60261aad39dfc5828d3bf805b2aa10de71913
Paul Mateos <mateosau@yahoo.com>
2020-01-16 12:40:55 +1100

    update scripts with exisitng user update

--- createUserWithGroupRolesPosts.py
+++ createUserWithGroupRolesPosts.py
@@ -22,6 +22,24 @@ def readJson(jsonfile):
     with open(jsonfile) as json_file:
         users = json.load(json_file)
     return users
+def getUserByName(name, header):
+    # get user id by name
+    getUserURI = protocol + "://" + baseurl + createUserurl + "/name/" + name
+    r = requests.get(url = getUserURI, headers = header) 
+    responseRoleJson = json.loads(r.text)
+    return responseRoleJson
+    
+def updateRole(user, header, userID):
+    for role in user["accountRoles"]:
+        # get role id by name
+        getRoleUserURI = protocol + "://" + baseurl + updateRoleUserURL + "name/" + role.replace(" ", "%20")
+        
+        r = requests.get(url = getRoleUserURI, headers = header) 
+        responseRoleJson = json.loads(r.text)
+
+        # add role to user
+        updateRoleUserURI = protocol + "://" + baseurl + updateRoleUserURL + str(responseRoleJson["id"]) + "/users/" + str(userID)
+        r = requests.put(url = updateRoleUserURI.replace(" ", "%20"), headers = header) 
 
 # create users in controller
 def createUsers(baseurl, protocol, accountName, userName, password, apiClient, apiSecret):
@@ -40,38 +58,24 @@ def createUsers(baseurl, protocol, accountName, userName, password, apiClient, a
         data['password'] = user["password"]
         
         try:
+            # create user
             r = requests.post(url = createUserURI, data =json.dumps(data), headers = header) 
 
             response = r.text
             print("Response is: %s"%response) 
             
-            # parse response to valid if application is creates successfully, validate applicationId
-            responseJson = json.loads(response)
-            if('id' not in responseJson):
+            if('id' not in response):
                 print("Error in creating users: %s" %users)
-
+            elif('User already present' in response):
+                print("User already present. Attempting to update Roles")
+                # parse response to valid if application is creates successfully, validate applicationId
+                userJson = getUserByName(data['name'], header)
+                userID = userJson["id"]
+                updateRole(user, header, userID)
             else:
-               print ("Successfully created users: %s" %users)
-               for group in user["groups"]:
-                    # get group id by name
-                    getGroupUserURI = protocol + "://" + baseurl + updateGroupUserURL + "name/" + group.replace(" ", "%20")
-                    r = requests.get(url = getGroupUserURI, headers = header) 
-                    responseGroupJson = json.loads(r.text)
-
-                    # add user to group
-                    updateGroupUserURI = protocol + "://" + baseurl + updateGroupUserURL + str(responseGroupJson["id"]) + "/users/" + str(responseJson["id"])
-                    r = requests.put(url = updateGroupUserURI, headers = header) 
-               for role in user["accountRoles"]:
-                    # get role id by name
-                    getRoleUserURI = protocol + "://" + baseurl + updateRoleUserURL + "name/" + role.replace(" ", "%20")
-                    
-                    r = requests.get(url = getRoleUserURI, headers = header) 
-                    responseRoleJson = json.loads(r.text)
-
-                    # add role to user
-                    updateRoleUserURI = protocol + "://" + baseurl + updateRoleUserURL + str(responseRoleJson["id"]) + "/users/" + str(responseJson["id"])
-                    r = requests.put(url = updateRoleUserURI.replace(" ", "%20"), headers = header) 
-
+                print ("Successfully created users: %s" %users)
+                responseJson = json.loads(response)
+                updateRole(user, header, responseJson["id"])
 
         except Exception as e:
             print(str(e))
@@ -93,13 +97,13 @@ if __name__ == "__main__":
     createUserurl="/controller/api/rbac/v1/users"
     updateGroupUserURL="/controller/api/rbac/v1/groups/"
     updateRoleUserURL="/controller/api/rbac/v1/roles/"
-    baseurl="platform-apacjantrainbp2arp-xwrpidox.srv.ravcloud.com:8090"
+    baseurl="enterpriseconsolep-vhpaulmateos-hmpczlps.srv.ravcloud.com:8090"
     protocol="http"
     accountName = "customer1"
-    userName = "appd"
+    userName = "admin"
     password = "appd"
-    apiClient = "Test"
-    apiSecret = "c3ab681f-9403-47a2-b41c-eca935092393"
+    apiClient = "apiclient"
+    apiSecret = "f19aab36-1a9c-483e-9749-a0cc255a0f55"
 
     # create user
     users = readJson(jsonfile)
